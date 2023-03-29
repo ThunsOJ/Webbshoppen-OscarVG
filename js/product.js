bcc4d7da8473126a155dc5a4c1a7c07ad348362d
ThunsOJ <oscar_jidaker@icloud.com>
2023-03-29 11:11:42 +0200

    Initial commit

---
+++ js/product.js
@@ -0,0 +1,11 @@
+//Product klass så att alla info om produkten sparas på vettigt sett
+export default class Product {
+    constructor(id, title, price, category, description, imageURL){
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.category = category;
+        this.description = description;
+        this.imageURL = imageURL;
+    }
+}
